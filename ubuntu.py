0250e25eb81c8c4b0cf1650cf2f88a18760a207f
Bhupender Sharma <sharma05031989@gmail.com>
2024-11-08 16:34:46 +0530

    Basic command set completed

---
+++ ubuntu.py
@@ -0,0 +1,88 @@
+"""
+# Linux Distribution ubuntu commands
+
+Download Virtual Box to create Linux based ubuntu environment
+
+Download ubuntu
+
+
+Ubuntu :- Distribution of Linux
+
+terminal : 
+
+commands
+
+$ ls  : Display all the files content list
+
+$ pwd : present working directory : Gives absolute Path
+
+$ / : Home
+
+$ mkdir bs Make directory
+
+$ touch 1.txt : makes blank file in the specified location
+
+$ mv 1.txt this/  move file 1.txt source to destination folder this
+
+$ cp 2.txt this/ copy file 2.txt source to destination folder this
+
+Users in Linux 
+
+1. Regular User : Home Directory : not able to access other users directory
+2. Root User/Super User : Full access : Able to access any other user directory 
+
+sudo : It defines the root user 
+
+3. Service User : apache server
+
+Paths in Linux
+
+Absolute path  : you can go to any arbitrary location : always starts with /
+Relative path
+
+Tree :   /root 
+   bin    etc     app
+
+$ sudo apt-get update : to fetch latest software
+
+$ sudo apt-get upgrade : to install latest software
+
+$ ls -R : List all the files in directory and subfiles . Works recursively
+
+$  touch .bs : if no initial before . , it considers it a hidden file.
+
+$ ls -a : list all files including hidden
+
+$ clear : clean the terminal screen
+
+$ history : shows the history
+
+$ echo bs : print bs
+
+$ ls -l : print files showcasing user group with permissions : -rw -r --r-- 1 : User Group Other
+
+To change read write permissions : we can use chmod calculator : Mathematics : Binary to decimal Conversion
+
+$ chmod 734 1.txt
+
+$ top : show the top most consuming processes
+
+$ ps -a : all processes
+
+$ kill 32731 : stop processes
+
+$ vim : text editor
+
+$ vim 1.txt
+
+press i to start typing
+
+press escape to get out from insert mode
+
+:q  - come out without saving
+:wq - come out with saving
+
+
+
+
+"""
